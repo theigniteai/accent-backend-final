a619c35585b24347ed0864a4ef398a4c1618511a
theigniteai <theignite.ai@gmail.com>
2025-05-09 15:58:40 +0400

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,11 @@
 // GET /profile, PUT /settings
+import express from "express";
+import { getProfile, updateSettings } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/profile", protect, getProfile);
+router.put("/settings", protect, updateSettings);
+
+export default router;
