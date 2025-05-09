b34c6cfeb714d1f8de63394c8b01815f8e15be40
theigniteai <theignite.ai@gmail.com>
2025-05-09 16:04:26 +0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,15 @@
 // user schema with assistantEnabled, voiceId
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  assistantEnabled: { type: Boolean, default: false },
+  voiceId: String,
+  subscription: { type: String, default: "Free" }
+}, {
+  timestamps: true
+});
+
+export default mongoose.model("User", userSchema);
