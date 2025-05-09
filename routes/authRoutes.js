d0c17202cc4a2a76186ea29591677baa4b6dc068
theigniteai <theignite.ai@gmail.com>
2025-05-09 19:04:23 +0400

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,10 @@
-// POST /signup, /login, /send-otp
+// routes/authRoutes.js
 import express from "express";
-import { signup, login, sendOtp } from "../controllers/authController.js";
+import { signup } from "../controllers/authController.js";
 
 const router = express.Router();
 
+// Signup route
 router.post("/signup", signup);
-router.post("/login", login);
-router.post("/send-otp", sendOtp);
 
 export default router;
