97347b9bf076218b6aec2fa01e0f1c251b5f77ba
theigniteai <theignite.ai@gmail.com>
2025-05-09 15:53:14 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+    console.log("✅ MongoDB Connected");
   } catch (err) {
-    console.error("DB Connection Failed:", err.message);
+    console.error("❌ MongoDB Connection Failed:", err.message);
     process.exit(1);
   }
 };
+
 export default connectDB;
